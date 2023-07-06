b5a1b7375a3e10a5b25c31dd51afa159066beab4
Mike Ackerman <dirtpathwanderer@gmail.com>
2023-07-06 10:19:37 -0600

    added capture histories

--- R/01_gather_data.R
+++ R/01_gather_data.R
@@ -14,38 +14,41 @@ library(PITcleanr)
 library(here)
 library(magrittr)
 
+#---------------------
+# gather PIT-tag data
+
 # get side configuration info from PTAGIS
 # config = buildConfig()
 # save(config, file = here("data/derived_data/config.rda"))
 load(here("data/derived_data/config.rda"))
 
-# GRA south fork clearwater sites
-sf_nodes = c("SC1",     # rkm 1; These rkms are from PTAGIS and I don't know their accuracy
-             "SC2",     # rkm 2   
-             "SC3",     # rkm 60
-             "SC4",     # rkm 81
-             "CRA")     # Crooked River IPTDS
+# GRA & south fork clearwater sites
+sf_nodes = c("SC1",  # rkm 1; These rkms are from PTAGIS and I don't know their accuracy
+             "SC2",  # rkm 2   
+             "SC3",  # rkm 60
+             "SC4",  # rkm 81
+             "CRA")  # Crooked River IPTDS
 
 sf_config = config %>%
   filter(node %in% c("GRA", sf_nodes))
 
-# get all observations from DART for adults at GRA and upstream
+# get all observations from DART for adults at GRA and upstream (includes newly and previously tagged fish)
 sthd_2022_obs = queryObsDART(species = "Steelhead",
                              loc = "GRA",
                              spawn_year = 2022) %>%
-  mutate(spawn_year = 2022)
+  mutate(spawn_year = 2022) %>%
+  group_by(tag_id) %>%
+  filter(any(obs_site %in% sf_nodes))
 
 sthd_2023_obs = queryObsDART(species = "Steelhead",
                              loc = "GRA",
                              spawn_year = 2023) %>%
-  mutate(spawn_year = 2023)
-
-# combine SY2022 and SY2023 and reduce to only those fish that were observed at a sf_node
-sf_dart_obs = rbind(sthd_2022_obs, sthd_2023_obs) %>%
+  mutate(spawn_year = 2023) %>%
   group_by(tag_id) %>%
   filter(any(obs_site %in% sf_nodes))
 
-# some cleaning of environment
+# combine SY2022 and SY2023 and reduce to only those fish that were observed at a sf_node
+sf_dart_obs = rbind(sthd_2022_obs, sthd_2023_obs)
 rm(sthd_2022_obs, sthd_2023_obs)
 
 # get lists of sy2022 and sy2023 steelhead that have been detected at sf_nodes
@@ -77,13 +80,11 @@ write.table(sf_2023_tags,
 # now query CTHs for sf_tags in PTAGIS
 
 # read in CTH file(s)
-cth_2022_path = here("data/derived_data/sfclrwtr_sy2022_sthd_cths.csv")
-cth_2023_path = here("data/derived_data/sfclrwtr_sy2023_sthd_cths.csv")
-sf_2022_cth = readCTH(cth_2022_path)
-sf_2023_cth = readCTH(cth_2023_path)
+sf_2022_cth = readCTH(here("data/derived_data/sfclrwtr_sy2022_sthd_cths.csv"))
+sf_2023_cth = readCTH(here("data/derived_data/sfclrwtr_sy2023_sthd_cths.csv"))
 
 # compress observations
-sf_2022_comp_cth = compress(cth_file = cth_2022_path,
+sf_2022_comp_cth = compress(cth_file = sf_2022_cth,
                             file_type = "PTAGIS",   
                             max_minutes = NA,
                             configuration = config,
@@ -91,7 +92,7 @@ sf_2022_comp_cth = compress(cth_file = cth_2022_path,
                             ignore_event_vs_release = TRUE) %>%
   mutate(spawn_year = 2022)
 
-sf_2023_comp_cth = compress(cth_file = cth_2023_path,
+sf_2023_comp_cth = compress(cth_file = sf_2023_cth,
                             file_type = "PTAGIS",   
                             max_minutes = NA,
                             configuration = config,
@@ -109,17 +110,51 @@ parent_child = tribble(~"parent", ~"child",
 
 # add nodes i.e., arrays
 pc_nodes = addParentChildNodes(parent_child, config)
+pc_nodes_order = c("SC1", "SC2", "SC3", "SC4B0", "SC4A0", "CRAB0", "CRAA0")
 
-# add directionality
-sf_2022_comp_cth = addDirection(sf_2022_comp_cth, pc_nodes)
-sf_2023_comp_cth = addDirection(sf_2023_comp_cth, pc_nodes)
+# add directionality and indicate whether each detection should be kept
+sf_2022_comp_cth = filterDetections(compress_obs = sf_2022_comp_cth,
+                                    parent_child = pc_nodes,
+                                    max_obs_date = NULL)
+sf_2023_comp_cth = filterDetections(compress_obs = sf_2023_comp_cth,
+                                    parent_child = pc_nodes,
+                                    max_obs_date = NULL)
 
 # merge compressed CTHs
-sf_comp_cth = rbind(sf_2022_comp_cth, sf_2023_comp_cth)
+sf_comp_cth = bind_rows(sf_2022_comp_cth, sf_2023_comp_cth) %>%
+  filter(auto_keep_obs == T) %>%
+  select(-direction,
+         -auto_keep_obs,
+         -user_keep_obs)
+
+# clean environment
+rm(sf_2022_comp_cth, sf_2023_comp_cth)
+
+# convert compress ptagis cths into capture histories
+sf_ch = sf_comp_cth %>%
+  filter(node %in% pc_nodes_order) %>%
+  mutate(node = factor(node,
+                       levels = pc_nodes_order)) %>%
+  select(tag_code, spawn_year, node) %>%
+  distinct() %>%
+  mutate(seen = 1) %>%
+  # convert to wide format
+  pivot_wider(names_from = node,
+              values_from = seen,
+              values_fill = 0,
+              names_sort = T,
+              names_expand = T) %>% # makes sure all nodes are included in ch's
+  # unite(ch, 2:(length(pc_nodes_order) + 1), sep = "")
+  left_join(sf_comp_cth %>%
+              group_by(tag_code, spawn_year, node) %>%
+              summarise(last_det = max(max_det)) %>%
+              pivot_wider(names_from = node,
+                          values_from = last_det,
+                          names_glue = "{node}_last_det"))
 
 # Get some biological data from LGR
 trap_df = read_csv("C:/Git/SnakeRiverFishStatus/data/LGTrappingDB/LGTrappingDB_2023-06-27.csv")
-sf_lgr_df = rbind(sf_2022_tags, sf_2023_tags) %>%
+sf_lgr_df = bind_rows(sf_2022_tags, sf_2023_tags) %>%
   distinct() %>%
   left_join(trap_df,
             by = c("tag_id" = "LGDNumPIT")) %>%
@@ -153,19 +188,24 @@ sf_lgr_df %<>%
 save(sf_lgr_df,
      sf_dart_obs,
      sf_comp_cth,
+     sf_ch,
      file = here("data/derived_data/sf_cleartwater_passage_data.rda"))
 
-# get stream gauge data using dataRetrieval package
+#------------------------
+# gather stream gage data
+
+# load necessary libraries
 library(remotes)
 # install_github("DOI-USGS/dataRetrieval",
 #                build_vignettes = TRUE, 
 #                build_opts = c("--no-resave-data",
 #                               "--no-manual"))
 library(dataRetrieval)
- 
-sf_elk_gage_info = readNWISsite(13337500) # sf clearwater river nr Elk City, ID
+
+# query stream gage data
+sf_elk_gage_info = readNWISsite(13337500)                       # sf clearwater river nr Elk City, ID
 sf_elk_gage_daily_cfs = readNWISdv(siteNumbers = 13337500,        
-                                   parameterCd = "00060",         # mean daily cfs
+                                   parameterCd = "00060",       # mean daily cfs
                                    startDate = "2021-07-01", 
                                    endDate = "2023-06-30") %>%
   rename(daily_mean_cfs = X_00060_00003)
